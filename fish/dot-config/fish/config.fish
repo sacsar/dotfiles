9ce064cd5c2ab97592f55a0b4ccc0330f137a68d
Sebastian Csar <sacsar@gmail.com>
2024-06-28 11:08:10 -0700

    Update fish config to find pyenv if it's already installed

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -6,6 +6,14 @@ set -x LOCAL_BIN $HOME/.local/bin
 
 fish_add_path $LOCAL_BIN
 
+# if we're switching to fish after installing pyenv, we need to make sure it's on our path
+if test -d $HOME/.pyenv
+    set -Ux PYENV_ROOT $HOME/.pyenv
+    set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+end
+
 # initialize various things if they're installed
 command -q starship; and starship init fish | source
+
+
 command -q pyenv; and pyenv init - | source
