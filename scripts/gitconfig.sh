550355d3229c79e7a46b6c5c1890763239cdff1e
Sebastian Csar <sacsar@gmail.com>
2024-08-14 13:43:32 -0700

    Add alias master as main to git config

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -2,3 +2,4 @@
 
 # Do this via script in case it's been set by IT on a work machine
 git config --global alias.adog "log --all --decorate --oneline --graph"
+git config --global alias.alias-master-as-main "!git symbolic-ref refs/heads/main refs/heads/master && git symbolic-ref refs/remotes/origin/main refs/remotes/origin/master && git switch main"
