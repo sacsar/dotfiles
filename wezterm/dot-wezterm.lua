b8da132ea507e439b2d1bcd2dcaa59014370dae1
Sebastian Csar <sacsar@gmail.com>
2024-11-30 14:31:35 -0800

    Change wezterm cursor to a bar

--- wezterm/dot-wezterm.lua
+++ wezterm/dot-wezterm.lua
@@ -10,5 +10,7 @@ config.color_scheme = 'nord'
 config.font = wezterm.font("FiraMono Nerd Font Mono", {weight="Regular", stretch="Normal", style="Normal"}) -- /Users/scsar/Library/Fonts/FiraMonoNerdFontMono-Regular.otf, CoreText
 config.font_size = 14
 
+config.default_cursor_style = 'SteadyBar'
+
 -- and finally, return the configuration to wezterm
 return config
