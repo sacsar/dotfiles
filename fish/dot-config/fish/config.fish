98ac4ce2db054a61ff1de836acd16b4da170062b
Sebastian Csar <sacsar@gmail.com>
2025-03-09 13:20:14 -0700

    Tidy up config.fish

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -6,6 +6,7 @@ set -x LOCAL_BIN $HOME/.local/bin
 
 fish_add_path $LOCAL_BIN
 fish_add_path /opt/nvim-linux64/bin
+
 # if we're switching to fish after installing pyenv, we need to make sure it's on our path
 if test -d $HOME/.pyenv
     set -Ux PYENV_ROOT $HOME/.pyenv
@@ -17,10 +18,14 @@ if test -d $HOME/.asdf
     source $HOME/.asdf/asdf.fish
 end
 
+if test -d "$HOME/.volta"
+    set -gx VOLTA_HOME "$HOME/.volta"
+    set -gx PATH "$VOLTA_HOME/bin" $PATH
+end
+
 # initialize various things if they're installed
 command -q starship; and starship init fish | source
-
 command -q pyenv; and pyenv init - | source
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-/home/sebastian/.local/bin/mise activate fish | source
+command -q mise; and mise activate fish | source
+
+
