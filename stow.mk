a00607d57a346b5084c25c1703dd553c2778b38d
Sebastian Csar <sacsar@gmail.com>
2025-04-22 07:37:37 -0700

    Switch to using cpanm for stow

--- stow.mk
+++ stow.mk
@@ -1,12 +1,16 @@
 STOW_VERSION := 2.4.0
 MIRROR_URL := https://mirrors.ocf.berkeley.edu/gnu/stow/
 PERL5LIB ?= $(HOME)/perl5/lib/perl5
+PERL5BIN ?= $(HOME)/perl5/bin
 
 CPANM = $(shell which cpanm)
-CPANM := $(if $(CPANM),$(CPANM),$(PERL5LIB)/cpanm)
+CPANM := $(if $(CPANM),$(CPANM),$(PERL5BIN)/cpanm)
 
 STOW = $(shell which stow)
 STOW := $(if $(STOW),$(STOW),$(XDG_BIN_HOME)/stow)
+
+$(CPANM):
+	curl -L https://cpanmin.us | perl - App::cpanminus
 
 /tmp/stow-$(STOW_VERSION).tar.gz:
 	curl -o $@ $(MIRROR_URL)stow-$(STOW_VERSION).tar.gz
@@ -18,5 +22,5 @@ STOW := $(if $(STOW),$(STOW),$(XDG_BIN_HOME)/stow)
 stow_install: $(STOW)
 
 $(XDG_BIN_HOME)/stow: /tmp/stow-$(STOW_VERSION)
-	cpan install Test::Output
-	cd $< && ./configure --prefix=$(HOME)/.local && $(MAKE) install
+	$(CPANM) install Test::Output
+	cd $< && ./configure --prefix=$(HOME)/.local --with-pmdir=$(PERL5LIB) && $(MAKE) install
