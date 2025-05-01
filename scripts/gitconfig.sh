afe3e6574e3110b3d7883911c4408bc8dbc8a2d5
Sebastian Csar <scsar@linkedin.com>
2025-05-01 11:13:24 -0700

    Set pull.rebase in gitconfig

--- scripts/gitconfig.sh
+++ scripts/gitconfig.sh
@@ -3,3 +3,4 @@
 # Do this via script in case it's been set by IT on a work machine
 git config --global alias.adog "log --all --decorate --oneline --graph"
 git config --global alias.alias-master-as-main "!git symbolic-ref refs/heads/main refs/heads/master && git symbolic-ref refs/remotes/origin/main refs/remotes/origin/master && git switch main"
+git config --global pull.rebase true
