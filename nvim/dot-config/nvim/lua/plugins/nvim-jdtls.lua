5f667682359a485d3a544cb4417a9d87be68d2c4
Sebastian Csar <sacsar@gmail.com>
2026-01-14 11:11:20 -0800

    Correct typo in jdtls config

--- nvim/dot-config/nvim/lua/plugins/nvim-jdtls.lua
+++ nvim/dot-config/nvim/lua/plugins/nvim-jdtls.lua
@@ -8,10 +8,15 @@ return {
   config = function()
     local root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" })
     local workspace_dir = vim.fn.fnamemodify(root_dir, ":p") -- make sure it's an absolute path
-    local data_dir = workspace_dir .. "/.jdlts"
+    local data_dir = workspace_dir .. "/.jdtls"
     local config = {
       cmd = { "jdtls", "-data", data_dir },
       init_options = {},
+      settings = {
+        java = {
+          format = { enabled = false },
+        },
+      },
     }
 
     require("jdtls").start_or_attach(config)
