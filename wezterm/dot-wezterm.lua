adfdc0b79ca7a72b5b34f89d75d2b95461548c63
Sebastian Csar <sacsar@gmail.com>
2026-02-08 18:49:00 -0800

    Make wezterm transparent in sway

--- wezterm/dot-wezterm.lua
+++ wezterm/dot-wezterm.lua
@@ -6,9 +6,15 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = "nord"
 config.font = wezterm.font("FiraMono Nerd Font Mono", { weight = "Regular", stretch = "Normal", style = "Normal" }) -- /Users/scsar/Library/Fonts/FiraMonoNerdFontMono-Regular.otf, CoreText
 config.font_size = 12
+
+if os.getenv("SWAYSOCK") then
+  -- match openSUSEway alacritty
+  config.window_background_opacity = 0.75
+else
+  config.color_scheme = "nord"
+end
 
 -- and finally, return the configuration to wezterm
 return config
